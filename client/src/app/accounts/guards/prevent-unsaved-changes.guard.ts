3acc25229930b3c2d3cf4f1e6b99024c8883ce27
tonycruz-dev <tonycruz-dev@outlook.com>
2020-12-06 18:48:43 +0000

    Update Save Gard

--- client/src/app/accounts/guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/accounts/guards/prevent-unsaved-changes.guard.ts
@@ -1,16 +1,19 @@
+import { CustomerEditComponent } from './../../customers/customer-edit/customer-edit.component';
 import { Injectable } from '@angular/core';
 import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ConfirmService } from 'src/app/shared/services/confirm.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(
-    component: unknown,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+  constructor(private confirmService: ConfirmService ) {}
+  canDeactivate( component: CustomerEditComponent): Observable<boolean>| boolean  {
+    // if (component.editForm.dirty) {
+    //   return this.confirmService.confirm();
+    // }
     return true;
   }
 
